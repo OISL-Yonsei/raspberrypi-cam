4bbb765580c7a793c05893f7c985ef3d61446cd3
Jaewoo Jung <lukael.jung@gmail.com>
2019-05-14 19:08:53 +0900

    Fix rgb2tiff.py

--- rgb2tiff.py
+++ rgb2tiff.py
@@ -2,10 +2,18 @@ import numpy as np
 import os
 import cv2
 from matplotlib import pyplot as plt
+from glob import glob
 
 path = os.getcwd()
 
-for filename in os.listdir(path):
+numpy_list = list()
+for numpy_name in glob('*.npy'):
+    numpy_list.append(numpy_name)
+
+for filename in numpy_list:
     npy = np.load(filename)
     BGR = cv2.cvtColor(npy, cv2.COLOR_RGB2BGR)
     cv2.imwrite(filename[:-4]+'.tiff', BGR)
+    print(filename + " converting end")
+
+os.system("Pause")
