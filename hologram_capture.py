04aa0e8ed2e612b98e3d34e9b8ebafc40cf190a3
Lukael <lukael.jung@gmail.com>
2019-08-27 18:07:07 +0900

    Fix hologram_capture

--- hologram_capture.py
+++ hologram_capture.py
@@ -1,3 +1,9 @@
+##################################
+# @author Lukael
+# @email lukael.jung@gmail.com
+# @create date 2019-07-26 11:41:07
+# @modify date 2019-07-26 11:41:07
+##################################
 import numpy as np
 import picamera
 import picamera.array 
@@ -9,7 +15,8 @@ time.sleep(0.5)
 picam.resolution = (1640,1232)
 # picam.resolution = picam.MAX_RESOLUTION
 picam.shutter_speed = 100
-picam.framerate = 30
+# picam.shutter_speed = 50
+picam.framerate = 30  
 time.sleep(2)
 picam.exposure_mode = 'off'
 picam.shutter_speed = picam.exposure_speed;
@@ -22,16 +29,22 @@ pibgr = picamera.array.PiRGBArray(picam)
 time.sleep(0.5)
 sizex = 640
 sizey = 480
+# sizex = 1920
+# sizey = 1080
 image = []
 while (True):
     picam.resolution = (1640,1232)
-    picam.capture(pibgr, "bgr", use_video_port = "True")
+    # picam.resolution = (640,480)
+    # picam.capture(pibgr, "bgr", use_video_port = "True")
+    picam.capture(pibgr, "bgr")
     frame = pibgr.array
     (h, w) = frame.shape[:2]
     frame_small = cv2.resize(frame, (sizex,sizey))
     frame_b, frame_g, frame_r = cv2.split(frame_small)
     frame_concat = np.hstack((frame_r, frame_g))
     cv2.imshow("live", frame_concat)
+    cv2.moveWindow("live",0,0)
+    # cv2.imshow("live", frame_r)
     key = cv2.waitKey(1) & 0xFF
     pibgr.truncate(0)
 
@@ -72,26 +85,28 @@ while (True):
 
     if key == ord("d"): #save multiple x20 image
         for i in range(1,21):
+        # for i in range(1,101):
             picam.resolution = picam.MAX_RESOLUTION
+            # picam.resolution = (1920,1080)
             picam.capture(pibgr, "bgr")
             image = pibgr.array
             timestr = time.strftime("%Y%m%d_%H%M%S");
             filename = "captured_bgr_{}.tiff".format(timestr)
             cv2.imwrite(filename, image)
-            print("[INFO] saved {}".format(filename))
+            print("[INFO] {} saved {}".format(i,filename))
             pibgr.truncate(0)
+        print("[INFO] Finish!")
 
     if key == ord("v"): #save bayer multiple x20 image
         for i in range(1,21):
             pibayer = picamera.array.PiBayerArray(picam)
             picam.capture(pibayer, "jpeg", bayer = "True")
             image = pibayer.array
-            # bayer_image = image.sum(axis=2)
-            # print(bayer_image.shape)
             timestr = time.strftime("%Y%m%d_%H%M%S");
             filename = "captured_bayer_{}.tiff".format(timestr)
             cv2.imwrite(filename, image)
-            print("[INFO] saved {}".format(filename))
+            print("[INFO] {} saved {}".format(i,filename))
             pibayer.truncate(0)
+        print("[INFO] Finish!")
 
 cv2.destroyAllWindows()
